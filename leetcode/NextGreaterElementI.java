e65a89d98ca63a9832b08aa4fbafe99818097b26
salah9abdelrahman <salah9abdelrahman@outlook.com>
2022-05-07 05:41:14 +0200

    nextGreaterElement

--- leetcode/NextGreaterElementI.java
+++ leetcode/NextGreaterElementI.java
@@ -2,6 +2,7 @@ package leetcode;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Stack;
 
 /**
  * https://leetcode.com/problems/next-greater-element-i/
@@ -10,6 +11,8 @@ import java.util.HashMap;
 public class NextGreaterElementI {
     public static void main(String[] args) {
         Arrays.stream(nextGreaterElement(new int[]{4, 1, 2}, new int[]{1, 3, 4, 2})).forEach(i -> System.out.print(i + " "));
+        System.out.println();
+        Arrays.stream(nextGreaterElement_monotonicStack(new int[]{4, 1, 2}, new int[]{1, 3, 4, 2})).forEach(i -> System.out.print(i + " "));
 
     }
 
@@ -35,4 +38,27 @@ public class NextGreaterElementI {
         }
         return res;
     }
+
+
+    public static int[] nextGreaterElement_monotonicStack(int[] nums1, int[] nums2) {
+        Stack<Integer> stack = new Stack<>();
+        // map works because it said in the problem it is a set, otherwise it won't
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int j : nums2) {
+            while (!stack.empty() && j > stack.peek()) {
+                map.put(stack.pop(), j);
+            }
+            stack.push(j);
+        }
+        while (!stack.empty()){
+            map.put(stack.pop(), -1);
+        }
+
+        int[] res = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            res[i] = map.get(nums1[i]);
+        }
+        return res;
+    }
+
 }
