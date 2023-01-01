d3842603a6bb18fecb74975ab50f063e5c1a0285
salahabdelrahman <salah9abdelrahman@outlook.com>
2023-01-01 07:40:23 +0200

    ValidSudoku another solution

--- leetcode/ValidSudoku.java
+++ leetcode/ValidSudoku.java
@@ -8,7 +8,7 @@ public class ValidSudoku {
     runtime O(N ^ 2)
     space O(N ^ 2)
      */
-    static boolean isValidSudoku(char[][] board) {
+    static boolean isValidSudoku_hashset(char[][] board) {
         int N = 9;
         HashSet<Character>[] rows = new HashSet[N];
         HashSet<Character>[] clos = new HashSet[N];
@@ -41,6 +41,49 @@ public class ValidSudoku {
                     return false;
                 }
                 boxes[idx].add(val);
+            }
+        }
+        return true;
+    }
+
+    /*
+ runtime O(N ^ 2)
+ space O(N ^ 2)
+  */
+    static boolean isValidSudoku_arrays(char[][] board) {
+        int N = 9;
+
+        // Use an array to record the status
+        int[][] rows = new int[N][N];
+        int[][] cols = new int[N][N];
+        int[][] boxes = new int[N][N];
+
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                // Check if the position is filled with number
+                if (board[r][c] == '.') {
+                    continue;
+                }
+                int pos = board[r][c] - '1';
+
+                // Check the row
+                if (rows[r][pos] == 1) {
+                    return false;
+                }
+                rows[r][pos] = 1;
+
+                // Check the column
+                if (cols[c][pos] == 1) {
+                    return false;
+                }
+                cols[c][pos] = 1;
+
+                // Check the box
+                int idx = (r / 3) * 3 + c / 3;
+                if (boxes[idx][pos] == 1) {
+                    return false;
+                }
+                boxes[idx][pos] = 1;
             }
         }
         return true;
