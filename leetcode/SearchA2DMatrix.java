f1734f33d6e4eeb78823e14ad1edd1ca8bf129d1
salahabdelrahman <salah9abdelrahman@outlook.com>
2023-04-18 08:44:05 +0200

    Search a 2D matrix another solution

--- leetcode/SearchA2DMatrix.java
+++ leetcode/SearchA2DMatrix.java
@@ -5,15 +5,45 @@ https://leetcode.com/problems/search-a-2d-matrix/description/?envType=study-plan
  */
 public class SearchA2DMatrix {
 
-  static   public boolean searchMatrix(int[][] matrix, int target) {
+    /*
+    Just less code solution
+    Time O(log(n * m))
+    Space O(1)
+     */
+    static public boolean searchMatrix2(int[][] matrix, int target) {
+        int min = 0;
+        int max = matrix.length * matrix[0].length - 1;
+        int n = matrix[0].length - 1;
+        while (min <= max) {
+            int midIdx = (min + max) / 2;
+
+            int midElement = matrix[midIdx / n][midIdx % n];
+            if (midElement > target) {
+                max = midIdx - 1;
+            } else if (midElement < target) {
+                min = midIdx + 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    ///////////////////////////////
+
+
+    /*
+    Time O(log(n * m))
+    Space O(1)
+     */
+    static public boolean searchMatrix(int[][] matrix, int target) {
         int arrRange = findArr(matrix, target);
         if (arrRange == -1) {
             return false;
         }
-        if(binarySearch(matrix[arrRange], target) == -1){
+        if (binarySearch(matrix[arrRange], target) == -1) {
             return false;
-        }
-        else {
+        } else {
             return true;
         }
     }
