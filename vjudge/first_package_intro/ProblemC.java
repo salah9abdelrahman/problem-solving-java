c8ddd5542612a07add46eecb98a6c32fb0514cb0
Salah Abdelrahman <salah9abdelrahman@outlook.com>
2024-02-09 23:52:37 +0200

    VOIS first assignment ProblemC

--- vjudge/first_package_intro/ProblemC.java
+++ vjudge/first_package_intro/ProblemC.java
@@ -1,4 +1,25 @@
 package vjudge.first_package_intro;
 
+import java.util.Scanner;
+
 public class ProblemC {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        int lower = 0;
+        int upper = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) < 97) {
+                upper++;
+            } else {
+                lower++;
+            }
+        }
+
+        if (lower >= upper) {
+            System.out.println(word.toLowerCase());
+        } else {
+            System.out.println(word.toUpperCase());
+        }
+    }
 }
