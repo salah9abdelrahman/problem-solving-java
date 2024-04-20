29996cb2d79b14e4092eb86a5e6a9776c312bede
Salah Abdelrahman <salah9abdelrahman@outlook.com>
2024-04-20 17:20:06 +0200

    patterns - sliding window - MinimumWindowSubstring

--- sliding_window/MinimumWindowSubstring.java
+++ sliding_window/MinimumWindowSubstring.java
@@ -64,65 +64,59 @@ public class MinimumWindowSubstring {
         }
 
         // Dictionary which keeps a count of all the unique characters in t.
-        Map<Character, Integer> dictT = new HashMap<Character, Integer>();
+        Map<Character, Integer> freqHashMapForT = new HashMap<Character, Integer>();
         for (int i = 0; i < t.length(); i++) {
-            int count = dictT.getOrDefault(t.charAt(i), 0);
-            dictT.put(t.charAt(i), count + 1);
+            freqHashMapForT.put(t.charAt(i),  freqHashMapForT.getOrDefault(t.charAt(i), 0) + 1);
         }
 
         // Number of unique characters in t, which need to be present in the desired window.
-        int required = dictT.size();
+        int required = freqHashMapForT.size();
 
         // Left and Right pointer
-        int l = 0, r = 0;
+        int start = 0, end = 0;
 
-        // formed is used to keep track of how many unique characters in t
-        // are present in the current window in its desired frequency.
-        // e.g. if t is "AABC" then the window must have two A's, one B and one C.
-        // Thus formed would be = 3 when all these conditions are met.
-        int formed = 0;
+        int current = 0;
 
         // Dictionary which keeps a count of all the unique characters in the current window.
-        Map<Character, Integer> windowCounts = new HashMap<Character, Integer>();
+        Map<Character, Integer> freqHashMapForS = new HashMap<Character, Integer>();
 
         // ans list of the form (window length, left, right)
         int[] ans = { -1, 0, 0 };
 
-        while (r < s.length()) {
+        while (end < s.length()) {
             // Add one character from the right to the window
-            char c = s.charAt(r);
-            int count = windowCounts.getOrDefault(c, 0);
-            windowCounts.put(c, count + 1);
+            char c = s.charAt(end);
+            freqHashMapForS.put(c, freqHashMapForS.getOrDefault(c, 0) + 1);
 
             // If the frequency of the current character added equals to the
             // desired count in t then increment the formed count by 1.
-            if (dictT.containsKey(c) && windowCounts.get(c).intValue() == dictT.get(c).intValue()) {
-                formed++;
+            if (freqHashMapForT.containsKey(c) && freqHashMapForS.get(c) == freqHashMapForT.get(c)) {
+                current++;
             }
 
             // Try and contract the window till the point where it ceases to be 'desirable'.
-            while (l <= r && formed == required) {
-                c = s.charAt(l);
+            while (start <= end && current == required) {
+                c = s.charAt(start);
                 // Save the smallest window until now.
-                if (ans[0] == -1 || r - l + 1 < ans[0]) {
-                    ans[0] = r - l + 1;
-                    ans[1] = l;
-                    ans[2] = r;
+                if (ans[0] == -1 || end - start + 1 < ans[0]) {
+                    ans[0] = end - start + 1;
+                    ans[1] = start;
+                    ans[2] = end;
                 }
 
                 // The character at the position pointed by the
                 // `Left` pointer is no longer a part of the window.
-                windowCounts.put(c, windowCounts.get(c) - 1);
-                if (dictT.containsKey(c) && windowCounts.get(c).intValue() < dictT.get(c).intValue()) {
-                    formed--;
+                freqHashMapForS.put(c, freqHashMapForS.get(c) - 1);
+                if (freqHashMapForT.containsKey(c) && freqHashMapForS.get(c).intValue() < freqHashMapForT.get(c).intValue()) {
+                    current--;
                 }
 
                 // Move the left pointer ahead, this would help to look for a new window.
-                l++;
+                start++;
             }
 
             // Keep expanding the window once we are done contracting.
-            r++;
+            end++;
         }
 
         return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
