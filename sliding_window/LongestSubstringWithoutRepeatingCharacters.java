26e84e3f2fe6d03c96bf17da79595b33744a3e36
Salah Abdelrahman <salah9abdelrahman@outlook.com>
2024-04-21 10:03:48 +0200

    patterns - sliding window - LongestSubstringWithoutRepeatingCharacters optimize

--- sliding_window/LongestSubstringWithoutRepeatingCharacters.java
+++ sliding_window/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,13 +1,14 @@
 package sliding_window;
 
 import java.util.HashMap;
+import java.util.Hashtable;
 
 /*
 https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
  */
 public class LongestSubstringWithoutRepeatingCharacters {
     public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstring("aabaab!bb"));
+        System.out.println(lengthOfLongestSubstring("au"));
     }
 
     static public int lengthOfLongestSubstring(String s) {
@@ -17,22 +18,25 @@ public class LongestSubstringWithoutRepeatingCharacters {
         int start = 0;
         int maxLen = 0;
         HashMap<Character, Integer> charIdx = new HashMap<>();
-
-        for (int end = 0; end < s.length(); end++) {
+        int end;
+        for (end = 0; end < s.length(); end++) {
             char c = s.charAt(end);
             if (!charIdx.containsKey(c)) {
                 charIdx.put(c, end);
-                if (end - start + 1 > maxLen) {
-                    maxLen = end - start + 1;
-                }
             } else {
-                start = charIdx.get(c) + 1;
-                charIdx.clear();
-                for (int i = start; i <= end; i++) {
-                    charIdx.put(s.charAt(i), i);
+                if (charIdx.get(c) >= start) {
+                    if (end - start > maxLen) {
+                        maxLen = end - start;
+                    }
+                    start = charIdx.get(c) + 1;
                 }
+                charIdx.replace(c, end);
             }
         }
-        return maxLen ;
+        if (maxLen < end - start) {
+            maxLen = end - start;
+        }
+        return maxLen;
     }
 }
+
