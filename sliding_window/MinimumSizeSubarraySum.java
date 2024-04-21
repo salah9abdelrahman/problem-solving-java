5936bf268cd6435fd2801a34f0f792feb4799bcc
Salah Abdelrahman <salah9abdelrahman@outlook.com>
2024-04-21 16:57:54 +0200

    patterns - sliding window - minSubArrayLen - refactor

--- sliding_window/MinimumSizeSubarraySum.java
+++ sliding_window/MinimumSizeSubarraySum.java
@@ -7,31 +7,19 @@ public class MinimumSizeSubarraySum {
 //        System.out.println(minSubArrayLen(15, new int[]{5,1,3,5,10,7,4,9,2,8}));
     }
 
-    static   public int minSubArrayLen(int target, int[] nums) {
+    static public int minSubArrayLen(int target, int[] nums) {
         int start = 0, end = 0, minLen = Integer.MAX_VALUE, sum = 0;
         for (end = 0; end < nums.length; end++) {
             sum += nums[end];
-            minLen = getMinLen(target, start, end, minLen, sum);
-            while (sum > target && start < end) {
+            while (sum >= target) {
+                minLen = Math.min(end + 1 - start, minLen);
                 sum -= nums[start];
                 start++;
-                minLen = getMinLen(target, start, end, minLen, sum);
             }
-        }
-        end--;
-        while (sum >= target && start < end) {
-            sum -= nums[start];
-            start++;
-            minLen = getMinLen(target, start, end, minLen, sum);
         }
         return minLen == Integer.MAX_VALUE ? 0 : minLen;
     }
 
-    private static int getMinLen(int target, int start, int end, int minLen, int sum) {
-        if (sum >= target && end - start + 1 < minLen) {
-            minLen = end - start + 1;
-        }
-        return minLen;
-    }
+
 
 }
