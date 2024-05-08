e4c9c87e18fed2361ebd026f317dd89afd88ed6d
Salah Abdelrahman <salah9abdelrahman@outlook.com>
2024-05-08 09:06:35 +0300

    patterns - Merge Intervals - TaskScheduler math

--- merge_intervals/TaskScheduler.java
+++ merge_intervals/TaskScheduler.java
@@ -112,8 +112,36 @@ Space : O(26) = O(1)
     }
 
 
+    /*
+Time complexity: O(N)
+Space : O(26) = O(1)
+*/
+    static public int leastInterval_math(char[] tasks, int n) {
+        // freq array to store the frequency of each task
+        int[] freq = new int[26];
+        int maxCount = 0;
+
+        // Count the frequency of each task and find the maximum frequency
+        for (char task : tasks) {
+            freq[task - 'A']++;
+            maxCount = Math.max(maxCount, freq[task - 'A']);
+        }
+
+        // Calculate the total time needed for execution
+        int time = (maxCount - 1) * (n + 1);
+        for (int f : freq) {
+            if (f == maxCount) {
+                time++;
+            }
+        }
+
+        // Return the maximum of total time needed and the length of the task list
+        return Math.max(tasks.length, time);
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(leastInterval_greedy(new char[]{'A', 'A', 'A', 'B', 'B', 'C', 'C', 'D',}, 3));
+        System.out.println(leastInterval_math(new char[]{'A', 'A', 'A', 'B', 'B', 'C', 'C', 'D',}, 3));
     }
 
 
