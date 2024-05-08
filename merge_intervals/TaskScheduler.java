9f6aa42a88d8b13ca2c9987498e262b1a21b6db9
Salah Abdelrahman <salah9abdelrahman@outlook.com>
2024-05-08 07:55:04 +0300

    patterns - Merge Intervals - TaskScheduler

--- merge_intervals/TaskScheduler.java
+++ merge_intervals/TaskScheduler.java
@@ -50,9 +50,40 @@ public class TaskScheduler {
         return time;
     }
 
+    /*
+Time complexity: O(N)
+Space : O(26) = O(1)
+ */
+    static public int leastInterval(char[] tasks, int n) {
+        // Create a frequency array to keep track of the count of each task
+        int[] freq = new int[26];
+        for (char task : tasks) {
+            freq[task - 'A']++;
+        }
+
+        // Sort the frequency array in non-decreasing order
+        Arrays.sort(freq);
+
+        // Calculate the maximum frequency of any task
+        int maxFreq = freq[25] - 1;
+
+        // Calculate the number of idle slots that will be required
+        int idleSlots = maxFreq * n;
+
+
+        // Iterate over the frequency array from the second-highest frequency to the lowest frequency
+        for (int i = 24; i >= 0 && freq[i] > 0 && idleSlots > 0; i--) {
+            // Subtract the minimum of the maximum frequency and the current frequency from the idle slots
+            idleSlots -= Math.min(maxFreq, freq[i]);
+        }
+        // If there are any idle slots left, add them to the total number of tasks
+        return idleSlots > 0 ? idleSlots + tasks.length : tasks.length;
+    }
+
+
 
     public static void main(String[] args) {
-        System.out.println(leastInterval_pq(new char[]{'A', 'A', 'A', 'B', 'B', 'B'}, 2));
+        System.out.println(leastInterval(new char[]{'A', 'A', 'C', 'C', 'C', 'B', 'E', 'E', 'E',}, 2));
     }
 
 
