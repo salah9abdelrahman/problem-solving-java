25010456a3a16cffa0c1545923f861728af266ee
Salah Abdelrahman <salah9abdelrahman@outlook.com>
2024-05-17 09:04:49 +0300

    patterns - In-Place Manipulation of a Linked List - 2074. Reverse Nodes in Even Length Groups

--- in_place_manipulation_of_linked_list/ReverseNodesInEvenLengthGroups.java
+++ in_place_manipulation_of_linked_list/ReverseNodesInEvenLengthGroups.java
@@ -5,6 +5,10 @@ https://leetcode.com/problems/reverse-nodes-in-even-length-groups/description/
  */
 public class ReverseNodesInEvenLengthGroups {
 
+    /*
+    Time O(n)
+    Space O(1)
+     */
 
     public static ListNode reverseEvenLengthGroups(ListNode head) {
         if (head == null || head.next == null) {
@@ -56,22 +60,51 @@ public class ReverseNodesInEvenLengthGroups {
         return head;
     }
 
+ static    public ListNode reverseEvenLengthGroups_2(ListNode head) {
+        int groupLen = 1;
+        ListNode curr = head;
+        ListNode prev = null;
+        while(curr != null){
+            int numOfNodes = 0;
+            ListNode orgCurr = curr;
+            ListNode orgPrev = prev;
+            while(curr != null && numOfNodes < groupLen){
+                prev = curr;
+                curr = curr.next;
+                numOfNodes++;
+            }
+
+            if(numOfNodes > 0 && numOfNodes % 2 == 0){
+                ListNode headOfGroup = reverse(orgCurr, numOfNodes);
+                orgPrev.next = headOfGroup;
+                // 1->2->3->4->5
+                // After first reverse
+                // 1->3->2->4->5
+                // 2 shoud be new prev
+                prev = orgCurr;
+            }
+            groupLen++;
+        }
+
+        return head;
+    }
+
     public static void main(String[] args) {
         ListNode head = new ListNode(1);
         head.next = new ListNode(2);
-//        head.next.next = new ListNode(3);
-//        head.next.next.next = new ListNode(4);
-//        head.next.next.next.next = new ListNode(5);
-//        head.next.next.next.next.next = new ListNode(6);
-//        head.next.next.next.next.next.next = new ListNode(7);
-//        head.next.next.next.next.next.next.next = new ListNode(8);
-//        head.next.next.next.next.next.next.next.next = new ListNode(9);
-//        head.next.next.next.next.next.next.next.next.next = new ListNode(10);
-//        head.next.next.next.next.next.next.next.next.next.next = new ListNode(11);
-//        head.next.next.next.next.next.next.next.next.next.next.next = new ListNode(12);
-//        head.next.next.next.next.next.next.next.next.next.next.next.next = new ListNode(13);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+        head.next.next.next.next.next.next = new ListNode(7);
+        head.next.next.next.next.next.next.next = new ListNode(8);
+        head.next.next.next.next.next.next.next.next = new ListNode(9);
+        head.next.next.next.next.next.next.next.next.next = new ListNode(10);
+        head.next.next.next.next.next.next.next.next.next.next = new ListNode(11);
+        head.next.next.next.next.next.next.next.next.next.next.next = new ListNode(12);
+        head.next.next.next.next.next.next.next.next.next.next.next.next = new ListNode(13);
 
-        head = reverseEvenLengthGroups(head);
+        head = reverseEvenLengthGroups_2(head);
         print(head);
     }
 
@@ -85,6 +118,25 @@ public class ReverseNodesInEvenLengthGroups {
             curr = nextTemp;
             count--;
         }
+        return prev;
+    }
+
+   static public ListNode reverse(ListNode head, int groupLen){
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = null;
+        while(curr != null && groupLen > 0){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            groupLen--;
+        }
+
+        // 1->2->3->4->5
+        // as 2 came as head of the group
+        // just setting up 2's next as 4
+        head.next = next;
         return prev;
     }
 
