5008bbb74d14a5a76e475d53fa73f5df1a83dd17
Salah Abdelrahman <salah9abdelrahman@outlook.com>
2024-07-21 02:09:30 +0300

    patterns - k-way merge - MergeSortedArray

---
+++ KWayMerge/KthSmallestElementInASortedMatrix.java
@@ -0,0 +1,55 @@
+package KWayMerge;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class KthSmallestElementInASortedMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                { 1, 5, 9 },
+                { 10, 11, 13 },
+                { 12, 13, 15 }
+        };
+        System.out.println(kthSmallest_usingHeapApproach(matrix, 8));
+    }
+
+    static public int kthSmallest_usingHeapApproach(int[][] matrix, int k) {
+        List<Integer> output = new ArrayList<>();
+
+        PriorityQueue<Element> minHeap = new PriorityQueue<>();
+
+        for (int i = 0; i < matrix.length; i++) {
+            minHeap.add(new Element(matrix[i][0], i, 0));
+        }
+
+        while (!minHeap.isEmpty()) {
+            Element e = minHeap.poll();
+            output.add(e.val);
+
+            if (e.idx < matrix[e.source].length - 1) {
+                minHeap.add(new Element(matrix[e.source][e.idx + 1], e.source, e.idx + 1));
+            }
+
+        }
+        System.out.println(output);
+        return output.get(k - 1);
+    }
+
+    static class Element implements Comparable<Element> {
+        public Element(int val, int source, int idx) {
+            this.val = val;
+            this.source = source;
+            this.idx = idx;
+        }
+
+        int val;
+        int source;
+        int idx;
+
+        @Override
+        public int compareTo(Element o) {
+            return this.val - o.val;
+        }
+    }
+}
--- KWayMerge/MergeKSortedArrays.java
+++ KWayMerge/MergeKSortedArrays.java
@@ -8,55 +8,34 @@ public class MergeKSortedArrays {
         List<Integer> l2 = Arrays.asList(3, 4, 7, 10);
         List<Integer> l3 = Arrays.asList(1, 4, 9);
         List<Integer> l4 = Arrays.asList(5, 10);
-        System.out.println((kMerge_usingHeapApproach(l1, l2, l3, l4)));
+        System.out.println(kMerge_usingHeapApproach(Arrays.asList(l1, l2, l3, l4)));
 
     }
 
-    static List<Integer> kMerge_usingHeapApproach(List<Integer> l1, List<Integer> l2, List<Integer> l3, List<Integer> l4) {
+    static List<Integer> kMerge_usingHeapApproach(List<List<Integer>> lists) {
         List<Integer> output = new ArrayList<>();
 
         PriorityQueue<Element> minHeap = new PriorityQueue<>();
 
-        minHeap.add(new Element(l1.get(0), 1, 0));
-        minHeap.add(new Element(l2.get(0), 2, 0));
-        minHeap.add(new Element(l3.get(0), 3, 0));
-        minHeap.add(new Element(l4.get(0), 4, 0));
-        int count = 0;
+        for (int i = 0; i < lists.size(); i++) {
+            minHeap.add(new Element(lists.get(i).get(0), i, 0));
+        }
 
-        while (count < 4) {
+        while (!minHeap.isEmpty()) {
             Element e = minHeap.poll();
 
             output.add(e.val);
-            if (e.source == 1) {
-                if (e.idx < l1.size() - 1) {
-                    minHeap.add(new Element(l1.get(e.idx + 1), 1, e.idx + 1));
-                } else {
-                    count++;
-                }
-            } else if (e.source == 2) {
-                if (e.idx < l2.size() - 1) {
-                    minHeap.add(new Element(l2.get(e.idx + 1), 2, e.idx + 1));
-                } else {
-                    count++;
-                }
-            } else if (e.source == 3) {
-                if (e.idx < l3.size() - 1) {
-                    minHeap.add(new Element(l3.get(e.idx + 1), 3, e.idx + 1));
-                } else {
-                    count++;
-                }
-            } else if (e.source == 4) {
-                if (e.idx < l4.size() - 1) {
-                    minHeap.add(new Element(l4.get(e.idx + 1), 4, e.idx + 1));
-                } else {
-                    count++;
-                }
-            }
-        }
 
+            if(e.idx < lists.get(e.source).size() - 1) {
+                minHeap.add(new Element(lists.get(e.source).get(e.idx + 1), e.source, e.idx + 1));
+            }
+           
+        }
 
         return output;
     }
+
+
 
     static class Element implements Comparable<Element> {
         public Element(int val, int source, int idx) {
