084ec75286dc4a0a7a9ede02f2f506e89c9045a0
Salah Abdelrahman <salah9abdelrahman@outlook.com>
2024-07-21 21:39:39 +0300

    patterns - k-way merge - KthSmallestElementInASortedMatrix - optimize

--- KWayMerge/KthSmallestElementInASortedMatrix.java
+++ KWayMerge/KthSmallestElementInASortedMatrix.java
@@ -7,9 +7,9 @@ import java.util.PriorityQueue;
 public class KthSmallestElementInASortedMatrix {
     public static void main(String[] args) {
         int[][] matrix = {
-                { 1, 5, 9 },
-                { 10, 11, 13 },
-                { 12, 13, 15 }
+                {1, 5, 9},
+                {10, 11, 13},
+                {12, 13, 15}
         };
         System.out.println(kthSmallest_usingHeapApproach(matrix, 8));
     }
@@ -17,20 +17,23 @@ public class KthSmallestElementInASortedMatrix {
     static public int kthSmallest_usingHeapApproach(int[][] matrix, int k) {
         List<Integer> output = new ArrayList<>();
 
-        PriorityQueue<Element> minHeap = new PriorityQueue<>();
+        int minNeededRows = Math.min(matrix.length, k);
 
-        for (int i = 0; i < matrix.length; i++) {
+        PriorityQueue<Element> minHeap = new PriorityQueue<>(minNeededRows);
+
+        for (int i = 0; i < minNeededRows; i++) {
             minHeap.add(new Element(matrix[i][0], i, 0));
         }
 
-        while (!minHeap.isEmpty()) {
+        int i = k;
+        while (i > 0) {
             Element e = minHeap.poll();
             output.add(e.val);
 
             if (e.idx < matrix[e.source].length - 1) {
                 minHeap.add(new Element(matrix[e.source][e.idx + 1], e.source, e.idx + 1));
             }
-
+            i--;
         }
         System.out.println(output);
         return output.get(k - 1);
