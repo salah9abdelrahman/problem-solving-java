abf35c413f2e3531e86c9dcd637831866823dfaa
Salah Abdelrahman <salah9abdelrahman@outlook.com>
2024-07-27 22:51:19 +0300

    patterns - Top K Elements - 973. K Closest Points to Origin

---
+++ top_k_elements/KClosestPointsToOrigin.java
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/k-closest-points-to-origin/description/
+package top_k_elements;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class KClosestPointsToOrigin {
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(kClosest(new int[][]{{-1, -3}, {-4, -5}, {-2, -2}, {-2, -3}}, 3)));
+    }
+
+    /**
+     * Time O(n log k)
+     *
+     * @param points
+     * @param k
+     * @return
+     */
+    static public int[][] kClosest(int[][] points, int k) {
+
+        PriorityQueue<int[]> heap = new PriorityQueue<>((p1, p2) -> Double.compare(calculate(p2), calculate(p1)));
+        for (int i = 0; i < k; i++) {
+            heap.add(points[i]);
+        }
+
+        for (int i = k; i < points.length; i++) {
+            if (Double.compare(calculate(points[i]), calculate(heap.peek())) < 0) {
+                heap.poll();
+                heap.add(points[i]);
+            }
+        }
+
+        return heap.toArray(new int[][]{});
+    }
+
+    static private double calculate(int[] p) {
+        double sqrt = Math.sqrt(p[0] * p[0] + p[1] * p[1]);
+        return sqrt;
+    }
+}
