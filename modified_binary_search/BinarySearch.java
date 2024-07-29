e6e554e39c2d29743d99076d8525c83f70039781
Salah Abdelrahman <salah9abdelrahman@outlook.com>
2024-07-29 06:08:35 +0300

    patterns - Modified Binary Search - 33. Search in Rotated Sorted Array

--- modified_binary_search/BinarySearch.java
+++ modified_binary_search/BinarySearch.java
@@ -19,6 +19,10 @@ class BinarySearchSolution {
         int start = 0;
         int end = nums.length - 1;
         while (start <= end) {
+            /*
+            Why not  mid = (start + end) /2?
+            to avoid the possibility of overflow from start + end if both of them are hugh numbers
+             */
             int mid = start + (end - start) / 2;
             if (nums[mid] == target) {
                 return mid;
