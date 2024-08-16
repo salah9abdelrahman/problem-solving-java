a9adf79c302b5f0475d689d4d13e29b0e443f4a2
Salah Abdelrahman <salah9abdelrahman@outlook.com>
2024-08-16 15:17:23 +0300

    patterns - Subsets - 78. Subsets

--- subsets/Subsets.java
+++ subsets/Subsets.java
@@ -8,6 +8,9 @@ public class Subsets {
     public static void main(String[] args) {
         System.out.println(new SubsetsCascadingSolution().subsets(new int[]{1, 2, 3}));
         System.out.println(new SubsetsBacktrackingSolution().subsets(new int[]{1, 2, 3}));
+
+        // cool solution
+        System.out.println(new BitMaskingSubsetsSolution().subsets(new int[]{1, 2, 3}));
     }
 }
 
@@ -37,7 +40,7 @@ class SubsetsBacktrackingSolution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> output = new ArrayList<>();
         for (int k = 0; k <= nums.length; k++) {
-            backtrack(output, nums, new ArrayList<Integer>(), 0, k);
+            backtrack(output, nums, new ArrayList<>(), 0, k);
         }
         return output;
     }
@@ -45,6 +48,7 @@ class SubsetsBacktrackingSolution {
     public void backtrack(List<List<Integer>> output, int[] nums, ArrayList<Integer> curr, int first, int k) {
         if (curr.size() == k) {
             output.add(new ArrayList<>(curr));
+            return;
         }
         for (int i = first; i < nums.length; i++) {
             curr.add(nums[i]);
@@ -52,5 +56,32 @@ class SubsetsBacktrackingSolution {
             curr.remove(curr.size() - 1);
         }
 
+    }
+}
+
+
+class BitMaskingSubsetsSolution {
+    /**
+     * Time O(N * (2 ** N))
+     * Space O(N)
+     */
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> output = new ArrayList<>();
+        int n = nums.length;
+
+        for (int i = (int) Math.pow(2, n); i < (int) Math.pow(2, n + 1); i++) {
+            // generate bitmask, from 0..00 to 1..11
+            String bitmask = Integer.toBinaryString(i).substring(1);
+
+            // append subset corresponding to that bitmask
+            List<Integer> curr = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                if (bitmask.charAt(j) == '1') {
+                    curr.add(nums[j]);
+                }
+            }
+            output.add(curr);
+        }
+        return output;
     }
 }
