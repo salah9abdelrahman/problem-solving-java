0fc63182bf0764dc805d5e54705934db71c2bd0e
salah9abdelrahman <salah9abdelrahman@outlook.com>
2024-12-07 19:22:05 +0200

    patterns - DP - 0/1 knapsack

--- dynamic_programming/knapsack.java
+++ dynamic_programming/knapsack.java
@@ -3,11 +3,15 @@ package dynamic_programming;
 
 public class knapsack {
     public static void main(String[] args) {
-        System.out.println(new Solution().findMaxKnapsackProfit(30, new int[]{10, 20, 30}, new int[]{22, 33, 44}));
+        System.out.println(new knapsack_bottomUp_spaceOptimize_Solution().findMaxKnapsackProfit(30, new int[]{10, 20, 30}, new int[]{22, 33, 44}));
     }
 }
 
-class Solution {
+/**
+ * @time O(n * m) values.len * capacity
+ * @space O(n * m)
+ */
+class knapsack_bottomUp_Solution {
     public int findMaxKnapsackProfit(int capacity, int[] weights, int[] values) {
         return bottomUp(capacity, weights, values);
     }
@@ -30,5 +34,44 @@ class Solution {
         return tabu[values.length][capacity];
     }
 }
+
+/**
+ * @time O(n * m) values.len * capacity
+ * @space O(m)
+ */
+class knapsack_bottomUp_spaceOptimize_Solution {
+    public int findMaxKnapsackProfit(int capacity, int[] weights, int[] values) {
+        return bottomUp(capacity, weights, values);
+    }
+
+    private static int bottomUp(int capacity, int[] weights, int[] values) {
+        /**
+         * tabu holds the prev row
+         * temp hold the current row
+         * at the end of compute each row, we copy it to dp
+         */
+        //
+        int[] tabu = new int[capacity + 1];
+        int[] temp = new int[capacity + 1];
+        // over items
+        for (int value = 1; value <= values.length; value++) {
+            // over weights
+            for (int weight = 1; weight <= capacity; weight++) {
+                if (weights[value - 1] <= weight) {
+                    temp[weight] = Math.max(tabu[weight], values[value - 1] + tabu[weight - weights[value - 1]]);
+                } else {
+                    temp[weight] = tabu[weight];
+                }
+            }
+
+            for (int i = 0; i < temp.length; i++) {
+                tabu[i] = temp[i];
+            }
+        }
+        return tabu[capacity];
+    }
+}
+
+
 
 
