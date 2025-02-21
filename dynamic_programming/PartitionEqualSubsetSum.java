deb2ab7e752ad940e2c5a7eefe6d4c3226795fa5
salah9abdelrahman <salah9abdelrahman@outlook.com>
2025-02-21 21:44:52 +0200

    patterns - DP - 416. Partition Equal Subset Sum - bottom up (brain damage fr)

--- dynamic_programming/PartitionEqualSubsetSum.java
+++ dynamic_programming/PartitionEqualSubsetSum.java
@@ -5,7 +5,8 @@ package dynamic_programming;
 public class PartitionEqualSubsetSum {
     public static void main(String[] args) {
 //        System.out.println(new PartitionEqualSubsetSumSolution_topDown().canPartition(new int[]{1, 5, 11, 5}));
-        System.out.println(new PartitionEqualSubsetSumSolution_topDown().canPartition(new int[]{1, 2, 3, 4}));
+//        System.out.println(new PartitionEqualSubsetSumSolution_topDown().canPartition(new int[]{1, 2, 3, 4}));
+        System.out.println(new PartitionEqualSubsetSumSolution_bottomUp().canPartition(new int[]{1, 2, 3, 4}));
 //        System.out.println(new PartitionEqualSubsetSumSolution_topDown().canPartition(new int[]{100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 99, 97}));
     }
 }
@@ -45,5 +46,33 @@ class PartitionEqualSubsetSumSolution_topDown {
         boolean result = dfs(arr, sum - arr[index], index + 1, memo) || dfs(arr, sum, index + 1, memo);
         memo[index][sum] = result;
         return result;
+    }
+}
+
+
+class PartitionEqualSubsetSumSolution_bottomUp {
+    public boolean canPartition(int[] nums) {
+        int totalSum = 0;
+        // find sum of all array elements
+        for (int num : nums) {
+            totalSum += num;
+        }
+        // if totalSum is odd, it cannot be partitioned into equal sum subset
+        if (totalSum % 2 != 0) return false;
+        int subSetSum = totalSum / 2;
+        int n = nums.length;
+        boolean[][] dp = new boolean[n + 1][subSetSum + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= n; i++) {
+            int curr = nums[i - 1];
+            for (int j = 0; j <= subSetSum; j++) {
+                if (j < curr)
+                    dp[i][j] = dp[i - 1][j];
+                else
+                    // without including curr || with including curr
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - curr];
+            }
+        }
+        return dp[n][subSetSum];
     }
 }
